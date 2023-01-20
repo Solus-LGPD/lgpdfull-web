df8d5c49f16da7f312e5a3d9b76296b5b85adf5b
Marcus Vinicius de M.Silva <marcus.silva@solus-it.local>
2023-01-20 16:05:20 -0300

    fix

--- src/views/lgpdfull/Lgpdfull.js
+++ src/views/lgpdfull/Lgpdfull.js
@@ -4,15 +4,18 @@ import { CButton, CButtonGroup, CLink } from "@coreui/react"
 const Lgpdfull = () => {
     return (
         <div  class="table-responsive-md d-flex justify-content-evenly" >
-            <CButtonGroup vertical role="group" class='mb-1 m-0 p-0 d-flex text-center flex-column ' aria-label="Vertical button group">
-                <CLink class='p-4pb-1 h3 text-decoration-none ' href="#" >Fase A</CLink>
+            
+            <CButtonGroup vertical role="group" class='mb-1 m-0 p-0 d-flex text-center flex-column w-25 ' aria-label="Vertical button group">
+                <CLink class='p-4pb-1 h1 text-decoration-none ' href="#" >A</CLink>
+                <CLink class='p-4pb-1 h5 text-decoration-none ' href="#" >Fase de Mapeamento</CLink>
                 <CButton color="primary mb-1 rounded-pill" href="http://www.quiz.solus-it.com.br/" target="__blank">Quiz LGPD</CButton>
                 <CButton color="primary mb-1 rounded-pill">Processos</CButton>
                 <CButton color="primary mb-1 rounded-pill" href="#">Inventário</CButton>
                 <CButton color="primary mb-1 rounded-pill" href="#">DPO</CButton>
             </CButtonGroup>
-            <CButtonGroup vertical role="group" aria-label="Vertical button group" class="text-center d-flex flex-column">
-                <CLink class='p-4pb-1 h3 text-decoration-none ' href="#" >Fase C</CLink>
+            <CButtonGroup vertical role="group" aria-label="Vertical button group" class="w-25 text-center d-flex flex-column">
+                <CLink class='p-4pb-1 h1 text-decoration-none ' href="#" >B</CLink>
+                <CLink class='p-4pb-1 h5 text-decoration-none ' href="#" >FASE DE IMPLEMENTAÇÃO</CLink>
                 <CButton color="primary mb-1 rounded-pill" href="#" disabled >Política de Priv.</CButton>
                 <CButton color="primary mb-1 rounded-pill" href="#" disabled>Ajustes de Contratos</CButton>
                 <CButton color="primary mb-1 rounded-pill" href="#" disabled>Termos de Uso</CButton>
@@ -21,8 +24,9 @@ const Lgpdfull = () => {
                 <CButton color="primary mb-1 rounded-pill" href="#" disabled>RIPD</CButton>
                 <CButton color="primary mb-1 rounded-pill" href="#" disabled>LIA</CButton>
             </CButtonGroup>
-            <CButtonGroup vertical role="group" aria-label="Vertical button group" class="text-center d-flex flex-column">
-                <CLink class='p-4pb-1 h3 text-decoration-none ' href="#" >Fase C</CLink>
+            <CButtonGroup vertical role="group" aria-label="Vertical button group" class="w-25 text-center d-flex flex-column">
+                <CLink class='p-4pb-1 h1 text-decoration-none ' href="#" >C</CLink>
+                <CLink class='p-4pb-1 h5 text-decoration-none ' href="#" >FASE DE IMPLEMENTAÇÃO AVANÇADA E MANUTENÇÃO</CLink>
                 <CButton color="primary mb-1 rounded-pill"  href="#" disabled>Comunicação c/ o Titular</CButton>
                 <CButton color="primary mb-1 rounded-pill"  href="#" disabled>Política de Segurança</CButton>
                 <CButton color="primary mb-1 rounded-pill"  href="#" disabled>Privacy by Design</CButton>
