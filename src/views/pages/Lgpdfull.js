ef9829eca31910c65a40812cb32d3738bc6abbb0
marcus silva <marvmms@gmail.com>
2023-04-04 17:57:38 -0300

    checklist

--- src/views/pages/Lgpdfull.js
+++ src/views/pages/Lgpdfull.js
@@ -1,13 +1,19 @@
 import { CButton, CButtonGroup} from "@coreui/react"
+import { useState } from "react"
 const Lgpdfull = () => {
+    const  [Check,setCheck] = useState(false);
+
+    const handleCheck = (e) => {
+        setCheck(e.target.value)
+    }
+    
     return (
-        
         <div className='position-relative  d-flex justify-content-evenly'>
             
             <h5 style={{color: "#2085c7", 'fontSize': '180px'}} className='justify-content-start flex-column d-flex'>A</h5>
             <CButtonGroup vertical role="group" aria-label="Vertical button group" className="position-relative gap-1 mt-0 m-3 w-25 text-center d-flex flex-column">
                 <h5 style={{color: "#2085c7", 'fontSize': '27px'}} className='p-4pb-1 h5 text-decoration-none ' >FASE DE MAPEAMENTO</h5>
-                <CButton style={{backgroundColor: "#2085c7"}} className="  border border-0 rounded-pill text-white" href="/quiz">Quiz LGPD</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}} className="  border border-0 rounded-pill text-white" href="/quiz">Quiz LGPD <input className="form-check-input " type="checkbox" value={Check} onClick={handleCheck} ></input></CButton>
                 <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/dpo">DPO</CButton>
                 <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/setor">Setor</CButton>
                 <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/inventario">Inventário</CButton>
@@ -21,7 +27,7 @@ const Lgpdfull = () => {
             <h5 style={{color: "#2085c7", 'fontSize': '180px'}} className=' justify-content-start flex-column d-flex'>B</h5>
             <CButtonGroup vertical role="group" aria-label="Vertical button group" className="position-relative gap-1 mt-0 m-3 w-25 text-center d-flex flex-column">
                 <h5 style={{color: "#2085c7", 'fontSize': '27px'}} className='position-relative p-4pb-1 h5 text-decoration-none ' >FASE DE IMPLEMENTAÇÃO</h5>
-                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="#" disabled >Política de Priv.</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}}  className=" border border-0 rounded-pill text-white" href="#" disabled >Política de Priv.</CButton>
                 <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="#" disabled>Ajustes de Contratos</CButton>
                 <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="#" disabled>Termos de Uso</CButton>
                 <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="#" disabled>Checklist de TI</CButton>
