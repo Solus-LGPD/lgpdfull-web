a9b21f6e01586328c8316fcbab2d4583e9370626
marcus silva <marvmms@gmail.com>
2023-04-10 12:04:27 -0300

    fix:rotas sistema

--- src/routes.js
+++ src/routes.js
@@ -19,13 +19,17 @@ const Ajuda = React.lazy(() => import('./views/pages/Help'));
 
 //FASE B
 const PoliticaDePrivacidade = React.lazy(() => import('./views/pages/PrivacyPolicy'));
+const TermosDeUso = React.lazy(() => import('./views/pages/UseTerms'));
+const PoliticaDeCookies = React.lazy(() => import('./views/pages/CookiesPolicy'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/lgpdfull', element: Lgpdfull },
+  { path: '/lgpdfull/ajuda', name: 'Ajuda', element: Ajuda },
   { path: '/lgpdfull/trocarsenha', name: 'Trocar de Senha', element: Trocarsenha },
   { path: '/lgpdfull/logout', name: 'Logout', element: Logout },
   { path: '/lgpdfull/perfil', name: 'Perfil', element: Perfil },
+  //Fase A
   { path: '/lgpdfull/inventario', name: 'Inventario', element: Inventario },
   { path: '/lgpdfull/inventario/criar', name: 'Criar', element: InventarioCriar },
   { path: '/lgpdfull/dpo', name: 'DPO', element: Dpo },
@@ -34,8 +38,10 @@ const routes = [
   { path: '/lgpdfull/setor/criar', name: 'Criar', element: SetorCriar },
   { path: '/lgpdfull/quiz', name: 'Quiz', element: Quiz },
   { path: '/lgpdfull/quiz/novo', name: 'Quiz', element: QuizCriar },
-  { path: '/lgpdfull/ajuda', name: 'Ajuda', element: Ajuda },
-  { path: '/lgpdfull/politica-de-privacidade', name: 'Política De Privacidade', element: PoliticaDePrivacidade }
+  //Fase B
+  { path: '/lgpdfull/politica-de-privacidade', name: 'Política De Privacidade', element: PoliticaDePrivacidade },
+  { path: '/lgpdfull/termos-de-uso', name: 'Termos De Uso', element: TermosDeUso },
+  { path: '/lgpdfull/politica-de-cookies', name: 'Politica de Cookies', element: PoliticaDeCookies },
 ]
 
 export default routes
