f2d3c634cf95a9bb3513c67fbdef3ffd2735c163
marcus silva <marvmms@gmail.com>
2023-04-13 17:51:36 -0300

    painel checklist

---
+++ src/views/pages/planB/chacklistTI/PanelChecklist.js
@@ -0,0 +1,26 @@
+const { CButtonGroup, CButton } = require("@coreui/react")
+
+const PenelChecklist = () => {
+    return (
+        <div className="d-flex justify-content-center">
+            <CButtonGroup className="position-relative gap-2 mt-0 m-3 w-25 text-center d-flex flex-column">
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="#">Introdução</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/chacklist/controle-de-acesso">Controle de Acesso</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/chacklist/dados-armazenados">Dados Armazenados</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/chacklist/seguranca-nas-comunicacoes">Segurança nas Comunicações</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/chacklist/gerenciamento-de-vulnerabilidade">Gerenciamento de Vulnerabilidade</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/chacklist/dispositivos-moveis">Dispositivos Móveis</CButton>
+            </CButtonGroup>
+            <CButtonGroup className="position-relative gap-2 mt-0 m-3 w-25 text-center d-flex flex-column">
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/chacklist/servicos-em-nuvem">Serviços em Nuvem</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/chacklist/cookies">Cookies</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/chacklist/politica-de-seguranca-da-informacao">Política de Segurança da Informação</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/chacklist/conscientizacao-e-treinamento">Conscientização e Treinamento</CButton>
+                <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/chacklist/contratos">Contratos</CButton>
+            </CButtonGroup>
+            
+        </div>
+    )
+}
+
+export default PenelChecklist;
