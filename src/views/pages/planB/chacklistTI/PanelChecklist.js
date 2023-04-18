06c94eb2dabf37b902215e33f4a4106aa3f01238
marcus silva <marvmms@gmail.com>
2023-04-18 15:20:26 -0300

    feat:button simple in panelchecklist

--- src/views/pages/planB/chacklistTI/PanelChecklist.js
+++ src/views/pages/planB/chacklistTI/PanelChecklist.js
@@ -1,25 +1,26 @@
-import { CButton, CButtonGroup } from '@coreui/react';
+import { CButton, CButtonGroup } from '@coreui/react'
+import {ButtonSimple } from '../../../../components/buttons/ButtonSimple'
 // import styles from '../../../../css/teste.css'
 
 const PenelChecklist = () => {
     return (
         <div>
-                <h2 className="d-flex justify-content-center mb-5">Painel Checklist</h2>
+            <h2 className="d-flex justify-content-center mb-5 text-black">Painel Checklist</h2>
             <div className="d-flex justify-content-center">
                 <CButtonGroup className="position-relative gap-2 mt-0 m-3 w-25 text-center d-flex flex-column">
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="#">Introdução</CButton>                    <></>
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/checklist/controle-de-acesso">Controle de Acesso</CButton>
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/checklist/dados-armazenados">Dados Armazenados</CButton>
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/checklist/seguranca-nas-comunicacoes">Segurança nas Comunicações</CButton>
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/checklist/gerenciamento-de-vulnerabilidade">Gerenciamento de Vulnerabilidade</CButton>
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/checklist/dispositivos-moveis">Dispositivos Móveis</CButton>
+                    <ButtonSimple href="#" label='Introdução'/>
+                    <ButtonSimple href="/lgpdfull/checklist/controle-de-acesso" label='Controle de Acesso'/>
+                    <ButtonSimple href="/lgpdfull/checklist/dados-armazenados"label='Dados Armazenados'/>
+                    <ButtonSimple href="/lgpdfull/checklist/seguranca-nas-comunicacoes" label='Segurança nas Comunicações'/>
+                    <ButtonSimple href="/lgpdfull/checklist/gerenciamento-de-vulnerabilidade" label='Gerenciamento de Vulnerabilidade'/>
+                    <ButtonSimple href="/lgpdfull/checklist/dispositivos-moveis" label='Dispositivos Móveis'/>
                 </CButtonGroup>
                 <CButtonGroup className="position-relative gap-2 mt-0 m-3 w-25 text-center d-flex flex-column">
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/checklist/servicos-em-nuvem">Serviços em Nuvem</CButton>
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/checklist/cookies">Cookies</CButton>
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/checklist/politica-de-seguranca-da-informacao">Política de Segurança da Informação</CButton>
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/checklist/conscientizacao-e-treinamento">Conscientização e Treinamento</CButton>
-                    <CButton style={{backgroundColor: "#2085c7"}} className=" border border-0 rounded-pill text-white" href="/lgpdfull/checklist/contratos">Contratos</CButton>
+                    <ButtonSimple href="/lgpdfull/checklist/servicos-em-nuvem"label='Serviços em Nuvem'/>
+                    <ButtonSimple href="/lgpdfull/checklist/cookies" label='Cookies'/>
+                    <ButtonSimple href="/lgpdfull/checklist/politica-de-seguranca-da-informacao" label='Política de Segurança da Informação'/>
+                    <ButtonSimple href="/lgpdfull/checklist/conscientizacao-e-treinamento" label='Conscientização e Treinamento'/>
+                    <ButtonSimple href="/lgpdfull/checklist/contratos" label='Contratos'/>
                 </CButtonGroup>
             </div>
         </div>
