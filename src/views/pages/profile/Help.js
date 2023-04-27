0b2f05063349e33c532bfb99263fd2d9c9190a5f
marcus silva <marvmms@gmail.com>
2023-04-27 17:15:44 -0300

    fix: plano B

--- src/views/pages/profile/Help.js
+++ src/views/pages/profile/Help.js
@@ -39,7 +39,7 @@ export default () => {
                         </CCardHeader>
                         {planA == true &&
                             <CCardBody >
-                                <CCardTitle>Quiz LGPD 1</CCardTitle>
+                                <CCardTitle>Quiz LGPD</CCardTitle>
                                 <CCardText>
                                     O Quiz LGPD serve para medir o nível atual de adequação da empresa com a Lei Geral de Proteção de Dados Pessoais (LPGP), Lei n° 13.709/2018.
                                     <br></br>
@@ -65,27 +65,25 @@ export default () => {
                         }
                         {planB == true &&
                             <CCardBody >
-                                <CCardTitle>Quiz LGPD 2</CCardTitle>
+                                <CCardTitle>Política de Privacidade</CCardTitle>
                                 <CCardText>
-                                    O Quiz LGPD serve para medir o nível atual de adequação da empresa com a Lei Geral de Proteção de Dados Pessoais (LPGP), Lei n° 13.709/2018.
+                                    Neste módulo é gerado um documento que tem como objetivo informar aos titulares dos dados como a empresa realiza o tratamento desses dados, quais dados são coletados, para que finalidades, quem tem acesso a eles, como são protegidos, entre outras informações relevantes.
                                     <br></br>
-                                    OBS: É Recomendado fazer o Quiz LPGD no início e no final do processo de adequação.
+                                </CCardText>
+                                <CCardTitle>Termo de Uso</CCardTitle>
+                                <CCardText>
+                                    Neste módulo é gerado um documento que tem como objetivo estabelece as condições e limitações de uso dos dados pessoais coletados por uma empresa, organização ou site. 
+                                    <br></br>
+                                </CCardText>
+                                <CCardTitle>Política de Cookies</CCardTitle>
+                                <CCardText>
+                                    Neste módulo é gerado um documento que tem como objetivo estabelece como os cookies são utilizados por um site, em conformidade com a Lei Geral de Proteção de Dados (LGPD) no Brasil.
+                                    <br></br>
                                 </CCardText>
 
-                                <CCardTitle>Encarregado dos Dados Pessoais (DPO)</CCardTitle>
+                                <CCardTitle>Checklist de TI</CCardTitle>
                                 <CCardText>
-                                    O Encarregado (DPO) é a pessoa indicada pelo controlador e operador para atuar como canal de comunicação entre o controlador, os titulares dos dados e a ANPD (Autoridade Nacional de Proteção de Dados)
-                                    O DPO pode ser pessoa física, jurídica, ou um comitê de pessoas.
-                                </CCardText>
-
-                                <CCardTitle>Setores da sua empresa</CCardTitle>
-                                <CCardText>
-                                    Neste módulo deve ser cadastrado todos os setores existentes na sua empresa.
-                                </CCardText>
-
-                                <CCardTitle>Inventário de Dados Pessoais</CCardTitle>
-                                <CCardText>
-                                    Neste módulo deve ser cadastrado a catalogação dos dados pessoais, fluxo, hipótese de tratamento e etc.
+                                    Neste módulo auxilia as empresas e organizações a verificarem se estão em conformidade com as exigências da Lei Geral de Proteção de Dados (LGPD) no âmbito de Tecnologia da Informação (TI).
                                 </CCardText>
                             </CCardBody>
                         }
